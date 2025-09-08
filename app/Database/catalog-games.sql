f080668dae826eeca2e59f777bd9d9ca98c8c5b8
arkanfauzan2008 <raffibwi38@gmail.com>
2025-09-09 06:45:24 +0700

    fixed modal content

--- app/Database/catalog-games.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE games (
-    game_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    release_year YEAR,
-    genre VARCHAR(100),
-    platform VARCHAR(100),
-    description TEXT
-);
