636e3bda9422ec206ba84209b284ddbb5dd07a61
Arkizan15 <raffibwi38@gmail.com>
2025-10-28 12:40:04 +0700

    add dashboard

---
+++ add_games.php
@@ -0,0 +1,10 @@
+<form action="">
+    <div>
+        <label for="name">Judul Games:</label>
+        <input type="text" id="name" name="name" required>
+    </div> 
+    <div>
+        <label for="genre">Genre:</label>
+        <input type="text" id="genre" name="genre" required>
+    </div> 
+</form>
